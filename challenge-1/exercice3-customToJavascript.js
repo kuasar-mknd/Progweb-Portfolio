d75404153b287d57e6bce747b80a2ef8b166e6d7
Samuel <samuel@mknd-records.com>
2022-09-20 11:04:59 +0200

    Exercice 3
    Custom to js : Add number to array and sort it

---
+++ challenge-1/exercice3-customToJavascript.js
@@ -0,0 +1,30 @@
+/* Consigne
+ * Ecrire une fonction permettant d'insérer un élément dans un tableau de 20 cases de sorte
+ * qu'après chaque insertion les valeurs du tableau soient toujours dans ordre croissant.
+ * Exemple :
+ * insertion de 5 => {5}
+ * insertion de 2 => {2, 5}
+ * insertion de 7 => {2, 5, 7}
+ * insertion de 1 => {1, 2, 5, 7}
+ * insertion de 3 => {1, 2, 3, 4, 7}
+ * ... et ainsi de suite
+*/
+
+
+function insertionDansTableau(tableau, element) {
+    tableau.push(element);
+    tableau.sort();
+}
+
+// Tests
+const tableau = [];
+insertionDansTableau(tableau, 5);
+console.log(tableau);
+insertionDansTableau(tableau, 2);
+console.log(tableau);
+insertionDansTableau(tableau, 7);
+console.log(tableau);
+insertionDansTableau(tableau, 1);
+console.log(tableau);
+insertionDansTableau(tableau, 3);
+console.log(tableau);
